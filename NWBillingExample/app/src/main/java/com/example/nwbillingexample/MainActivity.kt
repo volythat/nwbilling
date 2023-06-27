4756cfeb629e670ec58b7df66e1c9a1eb90c9fc8
Nguyen Kien Trung <trungnk@smartmove.com.vn>
2023-06-27 09:37:37 +0700

    change gradle follow jitpack

--- NWBillingExample/app/src/main/java/com/example/nwbillingexample/MainActivity.kt
+++ NWBillingExample/app/src/main/java/com/example/nwbillingexample/MainActivity.kt
@@ -4,29 +4,22 @@ import android.os.Bundle
 import android.util.Log
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.WindowCompat
-import com.android.billingclient.api.BillingClient
-import com.android.billingclient.api.BillingClient.ProductType
-import com.android.billingclient.api.BillingResult
-import com.android.billingclient.api.Purchase
 import com.example.nwbillingexample.databinding.ActivityMainBinding
-import com.newway.nwbillinglib.NWBilling
-import com.newway.nwbillinglib.NWBillingInterface
-import com.newway.nwbillinglib.NWProduct
-import com.newway.nwbillinglib.NWProductDetails
+
 
 
 class MainActivity : AppCompatActivity() {
 
     private lateinit var binding: ActivityMainBinding
-    var nwBilling : NWBilling? = null
+//    var nwBilling : NWBilling? = null
     val TAG = "MainActivity"
 
     //list products cần lấy
-    val products = listOf(
-        NWProduct("id subscription", ProductType.SUBS),
-        NWProduct("id subscription", ProductType.SUBS),
-        NWProduct("id onetime", ProductType.INAPP)
-    )
+//    val products = listOf(
+//        NWProduct("id subscription", ProductType.SUBS),
+//        NWProduct("id subscription", ProductType.SUBS),
+//        NWProduct("id onetime", ProductType.INAPP)
+//    )
 
     override fun onCreate(savedInstanceState: Bundle?) {
         WindowCompat.setDecorFitsSystemWindows(window, false)
@@ -34,94 +27,94 @@ class MainActivity : AppCompatActivity() {
 
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
-        setUpInApp()
+//        setUpInApp()
 
         binding.btnBuy.setOnClickListener {
-            buy()
+//            buy()
         }
     }
 
     override fun onDestroy() {
         super.onDestroy()
-        nwBilling?.destroy()
+//        nwBilling?.destroy()
     }
 
     //MARK: - FUN
-    private fun setUpInApp() {
-        nwBilling = NWBilling(this)
-        nwBilling?.listener = object : NWBillingInterface {
-            override fun onConnected() {
-                // đã kết nối xong với google play -> lấy thông tin products
-                nwBilling?.getInfo(products)
-
-                // các hàm lấy thông tin các product đã mua (gọi khi cần check xem user đã mua iap nào chưa)
-                nwBilling?.asyncInApp()
-                nwBilling?.asyncSubscription()
-            }
-
-            override fun onConnectFailed() {
-                // không kết nối được với billing google play
-            }
-
-            override fun onLoadedProductsInfo(
-                billingResult: BillingResult,
-                products:List<NWProductDetails>
-            ) {
-                //thông tin các product consumable , non-consumable (lifetime) sẽ trả về ở đây
-                products.forEach {product ->
-                    Log.e(TAG, "onLoadedProductsInfo: product = ${product.id} - price = ${product.formatPrice}")
-                }
-            }
-
-            override fun onLoadedSubscriptionInfo(
-                billingResult: BillingResult,
-                products:List<NWProductDetails>
-            ) {
-                //thông tin các product subscription sẽ trả về ở đây
-                products.forEach {product ->
-                    Log.e(TAG, "onLoadedSubscriptionInfo: product = ${product.id} - price = ${product.formatPrice}")
-                }
-            }
-
-            override fun onLoadPurchased(
-                billingResult: BillingResult,
-                purchases: List<Purchase>,
-                type: String
-            ) {
-                Log.e(TAG, "onLoadPurchased: size = ${purchases.size}")
-                if (billingResult.responseCode == BillingClient.BillingResponseCode.OK) {
-                    purchases.forEach {
-                        Log.e(TAG, "onLoadPurchased purchases.forEach: ${it.originalJson}")
-                    }
-                    if (purchases.isNotEmpty()){
-                        // Có ít nhất 1 purchase còn available
-                        Log.e(TAG, "onLoadPurchased: purchase available")
-                    }else{
-                        //không tìm thấy purchase nào
-                        Log.e(TAG, "onLoadPurchased: không tìm thấy purchase nào cả")
-                    }
-                }else{
-                    // load purchase lỗi => kiểm tra biến billingResult.responseCode xem là lỗi gì
-                    Log.e(TAG, "onPurchased: Failed")
-                    if (billingResult.responseCode == BillingClient.BillingResponseCode.USER_CANCELED){
-                        Log.e(TAG, "onPurchased: Cancel")
-                    }else if (billingResult.responseCode == BillingClient.BillingResponseCode.NETWORK_ERROR){
-                        Log.e(TAG, "onPurchased: NETWORK_ERROR")
-                    }else if (billingResult.responseCode == BillingClient.BillingResponseCode.BILLING_UNAVAILABLE){
-                        Log.e(TAG, "onPurchased: BILLING_UNAVAILABLE")
-                    }
-
-                }
-
-            }
-        }
-        nwBilling?.startServiceConnection()
-    }
-
-    //mua một iap nào đó
-    fun buy(){
-        val product = NWProduct("id sub",ProductType.SUBS)
-        nwBilling?.buy(this,product)
-        //=> kết quả sẽ trả về : onLoadPurchased
-    }
+//    private fun setUpInApp() {
+//        nwBilling = NWBilling(this)
+//        nwBilling?.listener = object : NWBillingInterface {
+//            override fun onConnected() {
+//                // đã kết nối xong với google play -> lấy thông tin products
+//                nwBilling?.getInfo(products)
+//
+//                // các hàm lấy thông tin các product đã mua (gọi khi cần check xem user đã mua iap nào chưa)
+//                nwBilling?.asyncInApp()
+//                nwBilling?.asyncSubscription()
+//            }
+//
+//            override fun onConnectFailed() {
+//                // không kết nối được với billing google play
+//            }
+//
+//            override fun onLoadedProductsInfo(
+//                billingResult: BillingResult,
+//                products:List<NWProductDetails>
+//            ) {
+//                //thông tin các product consumable , non-consumable (lifetime) sẽ trả về ở đây
+//                products.forEach {product ->
+//                    Log.e(TAG, "onLoadedProductsInfo: product = ${product.id} - price = ${product.formatPrice}")
+//                }
+//            }
+//
+//            override fun onLoadedSubscriptionInfo(
+//                billingResult: BillingResult,
+//                products:List<NWProductDetails>
+//            ) {
+//                //thông tin các product subscription sẽ trả về ở đây
+//                products.forEach {product ->
+//                    Log.e(TAG, "onLoadedSubscriptionInfo: product = ${product.id} - price = ${product.formatPrice}")
+//                }
+//            }
+//
+//            override fun onLoadPurchased(
+//                billingResult: BillingResult,
+//                purchases: List<Purchase>,
+//                type: String
+//            ) {
+//                Log.e(TAG, "onLoadPurchased: size = ${purchases.size}")
+//                if (billingResult.responseCode == BillingClient.BillingResponseCode.OK) {
+//                    purchases.forEach {
+//                        Log.e(TAG, "onLoadPurchased purchases.forEach: ${it.originalJson}")
+//                    }
+//                    if (purchases.isNotEmpty()){
+//                        // Có ít nhất 1 purchase còn available
+//                        Log.e(TAG, "onLoadPurchased: purchase available")
+//                    }else{
+//                        //không tìm thấy purchase nào
+//                        Log.e(TAG, "onLoadPurchased: không tìm thấy purchase nào cả")
+//                    }
+//                }else{
+//                    // load purchase lỗi => kiểm tra biến billingResult.responseCode xem là lỗi gì
+//                    Log.e(TAG, "onPurchased: Failed")
+//                    if (billingResult.responseCode == BillingClient.BillingResponseCode.USER_CANCELED){
+//                        Log.e(TAG, "onPurchased: Cancel")
+//                    }else if (billingResult.responseCode == BillingClient.BillingResponseCode.NETWORK_ERROR){
+//                        Log.e(TAG, "onPurchased: NETWORK_ERROR")
+//                    }else if (billingResult.responseCode == BillingClient.BillingResponseCode.BILLING_UNAVAILABLE){
+//                        Log.e(TAG, "onPurchased: BILLING_UNAVAILABLE")
+//                    }
+//
+//                }
+//
+//            }
+//        }
+//        nwBilling?.startServiceConnection()
+//    }
+//
+//    //mua một iap nào đó
+//    fun buy(){
+//        val product = NWProduct("id sub",ProductType.SUBS)
+//        nwBilling?.buy(this,product)
+//        //=> kết quả sẽ trả về : onLoadPurchased
+//    }
 }
